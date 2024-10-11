53286e3a634ea7706b9daccea29d1ac90cb64474
Belal Sryo <belalsryo@gmail.com>
2024-10-11 23:13:29 +0300

    updating main.py and jsonHandler

--- jsonHandler.py
+++ jsonHandler.py
@@ -5,51 +5,39 @@ class JsonHandler:
     email = ""
     password = ""
     
-    def __init__(self, name = "None", email = "example@gmail.com", password = "00000000"): 
+    def __init__(self, name="None", email="example@gmail.com", password="00000000"): 
         self.name = name
         self.email = email
         self.password = password
         
     def to_dict(self):
-        data = {"name": self.name, "email":self.email, "password": self.password}
-        return data
+        return {"name": self.name, "email": self.email, "password": self.password}
     
     def to_json(self):
-        data = self.to_dict()
-        str_data = json.dumps(data, indent=4)
-        return str_data
+        return json.dumps(self.to_dict(), indent=4)
     
-    def save_to_json(self, fileName = "data.json"):
-        
+    def save_to_json(self, fileName="data.json"):
         try:
             with open(fileName, "r") as file:
-    
                 content = json.load(file)
-        except(FileNotFoundError, json.JSONDecodeError):
+        except (FileNotFoundError, json.JSONDecodeError):
             content = []
-            
-        for name_chk in content:
-                if self.name == name_chk:
-                    file.close()
-                    return False
-                
-        file.close()
-        content.append(self.to_dict)
-        
+
+        for user in content:
+            if self.name == user.get("name") or self.email == user.get("email"):
+                return False  # Name already exists
+
+        content.append(self.to_dict())
+
         with open(fileName, "w") as file:
             json.dump(content, file, indent=4)
-            file.close()
-            
+
         return True
-    
 
 def check_user(fileName, userName, password):
-    flag = False
-        
     with open(fileName, "r") as file:
         content = json.load(file)
         for user in content:
             if user["name"] == userName and user["password"] == password:
-                flag = True
-        file.close()
-    return flag        
+                return True
+    return False
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask ,render_template
-
+from flask import Flask ,render_template, request, url_for, redirect
+import jsonHandler
 
 app = Flask(__name__)
+
+
 
 @app.route("/")
 def Home():
@@ -19,13 +21,32 @@ def Register():
 
 @app.route("/main")
 def Main():
-    # open json file
-    # check if the user is allready existed or not.
-    # append into json file.
-    # close the json file.
-    # display the main page.
-    # else redirect into an error page
-    return render_template("main.html")
+    id = request.args.get("page_id")
+    
+    if id == "reg_id":
+        name = request.args.get('newUserName')
+        email = request.args.get('newEmail')
+        password = request.args.get('newPassword')
+        print(name)
+        print(email)
+        print(password)
+        obj = jsonHandler.JsonHandler(name, email, password)
+        print(obj.to_json())
+        flag = obj.save_to_json("static/users.json")
+        if flag:
+            return render_template("main.html", DATA = obj.to_json())
+        else:
+            return render_template("index.html")
+    else:
+        name = request.args.get("userName")
+        password = request.args.get("password")
+        flag = jsonHandler.check_user("static/users.json", name, password)
+        print("Hello")
+        if flag:
+            return render_template("main.html", DATA = "Hello" + name)
+        else:
+            return render_template("index.html")
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
