8e82146e064a97aa75a57ee66cca663d70741f44
Belal Sryo <belalsryo@gmail.com>
2024-10-12 15:37:45 +0300

    applying POST method for login and register pages

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from flask import Flask ,render_template, request, url_for, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import jsonHandler
 
 app = Flask(__name__)
-
-
 
 @app.route("/")
 def Home():
@@ -13,40 +11,34 @@ def Home():
 def Login():
     return render_template("login.html")
 
-
 @app.route("/register")
 def Register():
     return render_template("register.html")
 
-
-@app.route("/main")
+@app.route("/main", methods=["GET", "POST"])  
 def Main():
-    id = request.args.get("page_id")
+    if request.method == "POST":  
+        id = request.form.get("page_id")  
+        if id == "reg_id":  
+            name = request.form.get('newUserName')
+            email = request.form.get('newEmail')
+            password = request.form.get('newPassword')
+            obj = jsonHandler.JsonHandler(name, email, password)
+            flag = obj.save_to_json("static/users.json")
+            if flag:
+                return render_template("main.html", DATA=obj.to_json())
+            else:
+                return render_template("index.html")
+        else:  # Login
+            name = request.form.get("userName")
+            password = request.form.get("password")
+            flag = jsonHandler.check_user("static/users.json", name, password)
+            if flag:
+                return render_template("main.html", DATA="Hello " + name)
+            else:
+                return render_template("index.html")
     
-    if id == "reg_id":
-        name = request.args.get('newUserName')
-        email = request.args.get('newEmail')
-        password = request.args.get('newPassword')
-        print(name)
-        print(email)
-        print(password)
-        obj = jsonHandler.JsonHandler(name, email, password)
-        print(obj.to_json())
-        flag = obj.save_to_json("static/users.json")
-        if flag:
-            return render_template("main.html", DATA = obj.to_json())
-        else:
-            return render_template("index.html")
-    else:
-        name = request.args.get("userName")
-        password = request.args.get("password")
-        flag = jsonHandler.check_user("static/users.json", name, password)
-        print("Hello")
-        if flag:
-            return render_template("main.html", DATA = "Hello" + name)
-        else:
-            return render_template("index.html")
-    
+    return redirect(url_for('Home')) 
 
 if __name__ == "__main__":
     app.run(debug=True)
