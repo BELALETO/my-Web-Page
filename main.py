06761e97a055636692de098bb114a97bb3604ee9
Belal Sryo <belalsryo@gmail.com>
2024-10-13 12:41:25 +0300

    mapping problems to thier files.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import jsonHandler
 import localCompiler
+import judge
 
 
 app = Flask(__name__)
@@ -65,18 +66,54 @@ def Quiz():
         content = request.args.get("problem_description")
         print(content)
         return render_template("quiz.html", QUIZ = content)
+    
+    
+    
 
 @app.route("/result", methods = ["GET", "POST"])
 def Result():
     code = request.form.get("code")
+    key = request.form.get("quiz_id") # temp
     print(code)
     cpp_file = "static/example.cpp"
     cpp_out = "example"
+    
+    # write a logic to map each file to its model solution.
+    main_file = ""
+    main_out = ""
+    print("Your key is: " + str(key))
+    
+    if key == "problem_1":
+        main_file = "static/exams/solution_1.cpp"
+        main_out = "solution_1"
+
+    elif key == "problem_2":
+        main_file = "static/exams/solution_1.cpp"
+        main_out = "solution_1"
+        
+    elif key == "problem_3":
+        main_file = "static/exams/solution_3.cpp"
+        main_out = "solution_3"
+        
+    elif key == "problem_4":
+        main_file = "static/exams/solution_4.cpp"
+        main_out = "solution_4"
+        
+    elif key == "problem_5":
+        main_file = "static/exams/solution_5.cpp"
+        main_out = "solution_5"
+    
+     
     with open(cpp_file, "w") as file:
         file.write(code)
-    obj = localCompiler.compiler(cpp_file, cpp_out)
-    print(obj.build_Run())
-    return render_template("result.html", DATA = code)
+    refree = judge.court(cpp_file, main_file, cpp_out, main_out)
+    if refree.sentence():
+        return render_template("result.html", DATA = "Your Solution is correct!", FLAG = key)
+    else:
+        return render_template("result.html", DATA = "Wrong Solution try again!", FLAG = key)
+    # print(refree.sentence())
+    # print(obj.build_Run())
+    
 
 
 if __name__ == "__main__":
--- static/example.cpp
+++ static/example.cpp
@@ -1,7 +1,1 @@
-#include<iostream>
-
-int main(void)
-{
-  std::cout << "hello safy" << std::endl;
-  return 0;
-}
+FDV
